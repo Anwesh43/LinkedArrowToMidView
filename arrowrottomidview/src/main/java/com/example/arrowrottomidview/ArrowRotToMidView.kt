6630f0cf77f2ff1a7c0f2aa417413519dda0efa0
Anwesh43 <anweshthecool0@gmail.com>
2021-05-29 01:51:01 +0530

    addng Renderer class

--- arrowrottomidview/src/main/java/com/example/arrowrottomidview/ArrowRotToMidView.kt
+++ arrowrottomidview/src/main/java/com/example/arrowrottomidview/ArrowRotToMidView.kt
@@ -191,4 +191,27 @@ class ArrowRotToMidView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArrowRotToMidView) {
+
+        private val animator : Animator = Animator(view)
+        private val arrowRotToMid : ArrowRotToMid = ArrowRotToMid(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            arrowRotToMid.draw(canvas, paint)
+            animator.animate {
+                arrowRotToMid.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            arrowRotToMid.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
