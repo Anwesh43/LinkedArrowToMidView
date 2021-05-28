b6c52405f3ddd155ecc885dcc71eaa7959d7f24d
Anwesh43 <anweshthecool0@gmail.com>
2021-05-29 01:38:15 +0530

    adding ARTMNode class

--- arrowrottomidview/src/main/java/com/example/arrowrottomidview/ArrowRotToMidView.kt
+++ arrowrottomidview/src/main/java/com/example/arrowrottomidview/ArrowRotToMidView.kt
@@ -127,4 +127,45 @@ class ArrowRotToMidView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ARTMNode(var i : Int, val state : State = State()) {
+
+        private var next : ARTMNode? = null
+        private var prev : ARTMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ARTMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawARTMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ARTMNode {
+            var curr : ARTMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
