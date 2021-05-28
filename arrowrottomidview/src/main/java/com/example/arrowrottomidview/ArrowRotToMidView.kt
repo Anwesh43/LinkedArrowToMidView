4a11d388a7f21b0226f61eeb41657825745ff581
Anwesh43 <anweshthecool0@gmail.com>
2021-05-29 01:39:53 +0530

    adding ArrowRotToMid class

--- arrowrottomidview/src/main/java/com/example/arrowrottomidview/ArrowRotToMidView.kt
+++ arrowrottomidview/src/main/java/com/example/arrowrottomidview/ArrowRotToMidView.kt
@@ -168,4 +168,27 @@ class ArrowRotToMidView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArrowRotToMid(var i : Int) {
+
+        private var curr : ARTMNode = ARTMNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
