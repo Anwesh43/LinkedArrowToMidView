859e3b69427d16ca644e34b44020f0e4c588fa04
Anwesh43 <anweshthecool0@gmail.com>
2021-05-29 02:08:24 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedarrowrottomidview/MainActivity.kt
+++ app/src/main/java/com/example/linkedarrowrottomidview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedarrowrottomidview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.arrowrottomidview.ArrowRotToMidView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ArrowRotToMidView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
