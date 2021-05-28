9033bc3608471fbf4ef2c931f47ae50c85099222
Anwesh43 <anweshthecool0@gmail.com>
2021-05-29 01:28:37 +0530

    adding Canvas extension methods

--- arrowrottomidview/src/main/java/com/example/arrowrottomidview/ArrowRotToMidView.kt
+++ arrowrottomidview/src/main/java/com/example/arrowrottomidview/ArrowRotToMidView.kt
@@ -24,8 +24,43 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val deg : Float = 45f
+val rot : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this -  i * n.inverse())
 fun Float.divideScale(i : Int,  n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawArrowRotToMid(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f  - 2 * j, 1f - 2 * j)
+        rotate(rot * sf4)
+        for (k in 0..1) {
+            save()
+            translate((w / 2 - size) * (1 - sf3) + size, 0f)
+            rotate((1f - k) * deg * sf2)
+            drawLine(0f, 0f, -size * sf1, 0f, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawARTMNode(i : Int, scale : Float, paint : Paint) {
+    val  w: Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArrowRotToMid(scale, w, h, paint)
+}
